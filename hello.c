78db2eaa03cf3f1495a6943313495cfd7f2b9371
Jayneel Vora <jayneelv68@gmail.com>
2020-02-18 20:34:06 -0800

    files from tutorial updated.

--- hello.c
+++ hello.c
@@ -1,4 +1,11 @@
-#include <stdio.h>
-void main() {
-	printf("hello\n");
+#include <linux/kernel.h>
+#include <stdlib.h>
+//asmlinkage defines to the compiler to look for parameters on the CPU stack instead of the registers.
+
+asmlinkage long sys_hello(void)
+{
+// print Hello! username
+// getenv from stdlib- for username.
+        printf("Hello %s\n",getenv("USER") );
+        return 0;
 }
