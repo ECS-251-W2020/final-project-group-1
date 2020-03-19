e44d290856d74bdfa7ba19f5593087ab050833a1
Jayneel Vora <jayneelv68@gmail.com>
2020-03-18 23:01:15 -0700

    ARC_basic setup

--- user_mode_code/Web-Server/src/page_cache.c
+++ user_mode_code/Web-Server/src/page_cache.c
@@ -31,6 +31,33 @@ int is_in_sec_cache(struct page *pge) {
   //}
   return 0;
 }
+int is_in_ghost1_cache(struct page *pge) {
+
+    struct ghost1_cache_entry *c_entry = ghost1_cache->first_entry;
+    while(c_entry!=NULL) {
+      if(c_entry->curr_page == pge){
+        //printf("in while\n");
+        return 1;
+      }
+      c_entry = c_entry->next_entry;
+    }
+  //}
+  return 0;
+}
+int is_in_ghost2_cache(struct page *pge) {
+
+    struct ghost1_cache_entry *c_entry = ghost1_cache->first_entry;
+    while(c_entry!=NULL) {
+      if(c_entry->curr_page == pge){
+        //printf("in while\n");
+        return 1;
+      }
+      c_entry = c_entry->next_entry;
+    }
+  //}
+  return 0;
+}
+
 void put_in_cache(struct page *pge) {
   struct p_cache_entry *c_entry = (struct p_cache_entry*) malloc(sizeof(struct p_cache_entry));
   c_entry->curr_page = pge;
@@ -76,13 +103,64 @@ void put_in_sec_cache(struct page *pge) {
             while(c_entry!=NULL) {
               if(c_entry->next_entry->next_entry == NULL){
                   c_entry->next_entry=NULL;
-                  p_cache->no_of_entries--;
+                  sec_cache->no_of_entries--;
               }
             }
-            put_in_cache(*pge);
+            put_in_sec_cache(*pge);
         }
 }
 
+void put_in_ghost1_cache(struct page *pge) {
+  struct ghost1_cache_entry *c_entry = (struct ghost1_cache_entry*) malloc(sizeof(struct ghost1_cache_entry));
+  c_entry->curr_page = pge;
+  c_entry->next_entry = NULL;
+  if(ghost1_cache->no_of_entries < ghost1_cache->cache_size) {
+      struct ghost1_cache_entry *temp = ghost1_cache->first_entry;
+      while(temp->next_entry!=NULL) {
+        temp=temp->next_entry;
+        //printf("in put here %s\n",pge->content);
+      }
+      //printf("in put2 here %s\n",pge->content);
+      temp->next_entry = c_entry;
+      ghost1_cache->no_of_entries+=1;
+    } else {
+            //remove last element.
+            struct ghost1_cache_entry *c_entry = ghost1_cache->first_entry;
+            while(c_entry!=NULL) {
+              if(c_entry->next_entry->next_entry == NULL){
+                  c_entry->next_entry=NULL;
+                  ghost1_cache->no_of_entries--;
+              }
+            }
+            put_in_ghost1_cache(*pge);
+        }
+}
+
+void put_in_ghost2_cache(struct page *pge) {
+  struct ghost2_cache_entry *c_entry = (struct ghost2_cache_entry*) malloc(sizeof(struct ghost2_cache_entry));
+  c_entry->curr_page = pge;
+  c_entry->next_entry = NULL;
+  if(ghost2_cache->no_of_entries < ghost2_cache->cache_size) {
+      struct ghost2_cache_entry *temp = ghost2_cache->first_entry;
+      while(temp->next_entry!=NULL) {
+        temp=temp->next_entry;
+        //printf("in put here %s\n",pge->content);
+      }
+      //printf("in put2 here %s\n",pge->content);
+      temp->next_entry = c_entry;
+      ghost2_cache->no_of_entries+=1;
+    } else {
+            //remove last element.
+            struct ghost2_cache_entry *c_entry = ghost2_cache->first_entry;
+            while(c_entry!=NULL) {
+              if(c_entry->next_entry->next_entry == NULL){
+                  c_entry->next_entry=NULL;
+                  ghost1_cache->no_of_entries--;
+              }
+            }
+            put_in_ghost2_cache(*pge);
+        }
+}
 
 void remove_from_cache(struct page *pge) {
     
@@ -101,7 +179,33 @@ void remove_from_cache(struct page *pge) {
   }
 
 
-  
+void remove_from_ghost1_cache(struct page *pge) {
+    
+    struct ghost1_cache_entry *c_entry = ghost1_cache->first_entry;
+      while(c_entry!=NULL) {
+        if(c_entry->curr_page == pge){
+            //remove the page.
+            c_entry= c_entry->next_page;
+        }
+        c_entry = c_entry->next_entry;
+      }
+    //}
+    return 0;
+  }
+
+void remove_from_ghost2_cache(struct page *pge) {
+    
+    struct ghost2_cache_entry *c_entry = ghost2_cache->first_entry;
+      while(c_entry!=NULL) {
+        if(c_entry->curr_page == pge){
+            //remove the page.
+            c_entry= c_entry->next_page;
+        }
+        c_entry = c_entry->next_entry;
+      }
+    //}
+    return 0;
+  }
 
 void remove_from_sec_cache(struct page *pge) {
     struct sec_cache_entry *c_entry = sec_cache->first_entry;
@@ -136,8 +240,24 @@ struct sec_cache* setup_sec_cache(struct page *pge) {
   sec_c->cache_size = 20;
   return sec_c;
 }
-void
-int main()
-{
-    return 0;
+
+struct sec_cache* setup_ghost1_cache(struct page *pge) {
+  struct ghost1_cache_entry *entr = (struct ghost2_cache_entry*) malloc(sizeof(struct ghost1_cache_entry));
+  struct page_cache *ghost1_c = (struct ghost1_cache*) malloc(sizeof(struct ghost1_cache));
+  entr->curr_page = pge;
+  entr->next_entry = NULL;
+  sec_c->first_entry = entr;
+  sec_c->no_of_entries = 1;
+  sec_c->cache_size = 20;
+  return sec_c;
+}
+struct sec_cache* setup_ghost2_cache(struct page *pge) {
+  struct ghost2_cache_entry *entr = (struct ghost2_cache_entry*) malloc(sizeof(struct ghost2_cache_entry));
+  struct page_cache *ghost2_c = (struct ghost2_cache*) malloc(sizeof(struct ghost2_cache));
+  entr->curr_page = pge;
+  entr->next_entry = NULL;
+  sec_c->first_entry = entr;
+  sec_c->no_of_entries = 1;
+  sec_c->cache_size = 20;
+  return sec_c;
 }
--- user_mode_code/Web-Server/src/page_cache.h
+++ user_mode_code/Web-Server/src/page_cache.h
@@ -6,12 +6,27 @@ struct page_cache {
   int no_of_entries;
 };
 
+// for 2Q, it will act as the secondary cache.
 struct sec_cache {
   struct sec_cache_entry *first_entry;
   int cache_size;
   int no_of_entries;
 };
-
+struct ghost1_cache {
+  struct sec_cache_entry *first_entry;
+  int cache_size;
+  int no_of_entries;
+};
+struct ghost2_cache {
+  struct sec_cache_entry *first_entry;
+  int cache_size;
+  int no_of_entries;
+};
+/*  for ARC, page_cache acts as L1,
+ sec_cache as L2.
+ghost1_cache as B1
+ ghost2_cache as B2.
+*/
 struct p_cache_entry {
   struct page *curr_page;
   struct p_cache_entry *next_entry;
@@ -21,16 +36,36 @@ struct sec_cache_entry {
   struct page *curr_page;
   struct p_cache_entry *next_entry;
 };
+struct ghost1_cache_entry {
+  struct page *curr_page;
+  struct p_cache_entry *next_entry;
+};
+struct ghost2_cache_entry {
+  struct page *curr_page;
+  struct p_cache_entry *next_entry;
+};
 
 extern struct page_cache *p_cache;
 extern struct sec_cache *sec_cache;
+extern struct ghost1_cache *ghost1_cache;
+extern struct ghost2_cache *ghost2_cache;
 int is_in_cache(struct page *pge);
 int is_in_sec_cache(struct page *pge)
+int is_in_ghost1_cache(struct page *pge)
+int is_in_ghost2_cache(struct page *pge)
+
 void put_in_cache(struct page *pge);
 void put_in_sec_cache(struct page *pge);
+void put_in_ghost1_cache(struct page *pge);
+void put_in_ghost2_cache(struct page *pge);
 
 void remove_from_cache(struct page *pge);
 void remove_from_sec_cache(struct page *pge);
+void remove_from_ghost1_cache(struct page *pge);
+void remove_from_ghost2_cache(struct page *pge);
+
 struct page_cache* setup_cache(struct page *pge);
 struct page_cache* setup_sec_cache(struct page *pge);
+struct page_cache* setup_ghost1_cache(struct page *pge);
+struct page_cache* setup_ghost2_cache(struct page *pge);
 #endif
